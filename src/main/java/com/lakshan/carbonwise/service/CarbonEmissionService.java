876807f5d1600f32dbb2663f03591e6ae248e372
Lakshan <lakshan.imantha02@gmail.com>
2025-06-12 01:10:00 +0530

    Add CarbonEmissionService to integrate with CarbonSutra API for carbon footprint estimation

--- src/main/java/com/lakshan/carbonwise/service/CarbonEmissionService.java
+++ src/main/java/com/lakshan/carbonwise/service/CarbonEmissionService.java
@@ -1,39 +1,59 @@
 package com.lakshan.carbonwise.service;
 
-import com.lakshan.carbonwise.config.CarbonInterfaceConfig;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lakshan.carbonwise.config.CarbonSutraConfig;
+import com.lakshan.carbonwise.model.CarbonElectricityRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 public class CarbonEmissionService {
 
-    private final CarbonInterfaceConfig carbonInterfaceConfig;
+    private final CarbonSutraConfig carbonSutraConfig;
     private final RestTemplate restTemplate;
 
     @Autowired
-    public CarbonEmissionService(CarbonInterfaceConfig carbonInterfaceConfig, RestTemplateBuilder restTemplateBuilder) {
-        this.carbonInterfaceConfig = carbonInterfaceConfig;
+    public CarbonEmissionService(CarbonSutraConfig carbonSutraConfig, RestTemplateBuilder restTemplateBuilder) {
+        this.carbonSutraConfig = carbonSutraConfig;
         this.restTemplate = restTemplateBuilder.build();
+
     }
 
-    public String calculateCarbonEmissions(String body) {
+    public double calculateEnergyEmissions(CarbonElectricityRequest carbonElectricityRequest) {
+        String electricityApiUrl = "https://carbonsutra1.p.rapidapi.com/electricity_estimate";
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + carbonInterfaceConfig.getApiKey());
-        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+        headers.set("Authorization", "Bearer " + carbonSutraConfig.getBearerAPIKey());
+        headers.set("X-RapidAPI-Key", carbonSutraConfig.getRapidAPIKey());
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
-        ResponseEntity<String> response = restTemplate.exchange(
-                "https://www.carboninterface.com/api/v1/emissions_calculations",
-                HttpMethod.POST,
-                entity,
-                String.class
-        );
-        return response.getBody();
+        String requestBody = carbonElectricityRequest.buildElectricityAPIRequest();
+        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        try {
+            String response = restTemplate.postForObject(
+                    electricityApiUrl,
+                    requestEntity,
+                    String.class
+            );
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode.get("data").get("co2e_kg").asDouble();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
+
 }
