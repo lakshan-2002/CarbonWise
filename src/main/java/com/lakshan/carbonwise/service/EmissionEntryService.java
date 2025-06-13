08e9d64c69fe00ddcf034f4f39715bf9139d7300
Lakshan <lakshan.imantha02@gmail.com>
2025-06-13 17:55:13 +0530

    Modify EmissionEntryService class

--- src/main/java/com/lakshan/carbonwise/service/EmissionEntryService.java
+++ src/main/java/com/lakshan/carbonwise/service/EmissionEntryService.java
@@ -1,6 +1,7 @@
 package com.lakshan.carbonwise.service;
 
 import com.lakshan.carbonwise.entity.EmissionEntry;
+import com.lakshan.carbonwise.model.CarbonRequestMapper;
 import com.lakshan.carbonwise.repository.EmissionEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,27 @@ import java.util.List;
 public class EmissionEntryService {
 
     private final EmissionEntryRepository emissionEntryRepository;
+    private final CarbonEmissionService carbonEmissionService;
+    private final CarbonRequestMapper carbonRequestMapper;
+    private final CarbonCalculationService carbonCalculationService;
 
     @Autowired
-    public EmissionEntryService(EmissionEntryRepository emissionEntryRepository) {
+    public EmissionEntryService(EmissionEntryRepository emissionEntryRepository,
+                                CarbonEmissionService carbonEmissionService,
+                                CarbonRequestMapper carbonRequestMapper, CarbonCalculationService carbonCalculationService
+    ) {
         this.emissionEntryRepository = emissionEntryRepository;
+        this.carbonEmissionService = carbonEmissionService;
+        this.carbonRequestMapper = carbonRequestMapper;
+        this.carbonCalculationService = carbonCalculationService;
     }
 
     public void addNewEmissionEntry(EmissionEntry emissionEntry) {
+        if(emissionEntry.getType().equals("Energy"))
+            carbonCalculationService.calculateEnergyEmissions(emissionEntry);
+        else if (emissionEntry.getType().equals("Transportation")) {
+
+        }
         emissionEntryRepository.save(emissionEntry);
     }
 
@@ -26,18 +41,30 @@ public class EmissionEntryService {
     }
 
     public EmissionEntry getEmissionEntryById(int id) {
-        return emissionEntryRepository.findById(id).orElseThrow(() -> new RuntimeException("No emission entry found with id: " + id));
+        return emissionEntryRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("No emission entry found with id: " + id));
     }
 
     public void updateEmissionEntry(EmissionEntry emissionEntry) throws RuntimeException {
-        if(emissionEntryRepository.existsById(emissionEntry.getId()))
+        if (emissionEntryRepository.existsById(emissionEntry.getId())) {
+            if (emissionEntry.getType().equals("Energy")) {
+                var carbonElectricityRequest = carbonRequestMapper.toElectricityRequest(emissionEntry);
+                double co2Factor = carbonEmissionService.
+                        calculateElectricityEmissions(carbonElectricityRequest);
+
+                emissionEntry.setCo2Emission(co2Factor);
+            }
+            if (emissionEntry.getData().equals("Natural Gas")) {
+                double co2Emission = 53.06 * emissionEntry.getAmount();
+                emissionEntry.setCo2Emission(co2Emission);
+            }
             emissionEntryRepository.save(emissionEntry);
-        else
+        } else
             throw new RuntimeException("No emission entry found with id: " + emissionEntry.getId());
     }
 
     public void deleteEmissionEntry(int id) {
-        if(emissionEntryRepository.existsById(id))
+        if (emissionEntryRepository.existsById(id))
             emissionEntryRepository.deleteById(id);
         else
             throw new RuntimeException("No emission entry found with id: " + id);
