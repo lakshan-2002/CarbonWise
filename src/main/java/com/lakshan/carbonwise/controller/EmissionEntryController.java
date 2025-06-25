b04b6f87bb59ffedbf0a2f59aa86170d70164186
Lakshan <lakshan.imantha02@gmail.com>
2025-06-25 21:25:03 +0530

    Enable CORS for all origins using @CrossOrigin annotation

--- src/main/java/com/lakshan/carbonwise/controller/EmissionEntryController.java
+++ src/main/java/com/lakshan/carbonwise/controller/EmissionEntryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/emission_entries")
 public class EmissionEntryController {
