0fb0c1bf46539db3095bd155c0dc3076242e14d5
Lakshan <lakshan.imantha02@gmail.com>
2025-07-01 09:49:30 +0530

    Implement endpoint to fetch recommendations using emission entry ID

--- src/main/java/com/lakshan/carbonwise/repository/RecommendationRepository.java
+++ src/main/java/com/lakshan/carbonwise/repository/RecommendationRepository.java
@@ -4,6 +4,11 @@ import com.lakshan.carbonwise.entity.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RecommendationRepository extends JpaRepository<Recommendation, Integer> {
+      List<Recommendation> findByEmissionEntryId(int emissionId);
+
 }
