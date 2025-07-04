cd7dc8ee325b5df51cfba8c06d57c82ce95dab86
Yasith Piyarathne <45797225+pandorian7@users.noreply.github.com>
2025-07-04 12:16:25 +0530

    Revert "Yasith auth system"

--- src/main/java/com/lakshan/carbonwise/repository/UserRepository.java
+++ src/main/java/com/lakshan/carbonwise/repository/UserRepository.java
@@ -4,11 +4,8 @@ import com.lakshan.carbonwise.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByEmail(String email);
-    Optional<User> findByEmailAndPassword(String email, String password);
 }
